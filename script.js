d362fd20cc9c417a49958860a463ccf68df81fe3
thisen@gmail.com <thisen@gmail.com>
2019-11-16 12:52:05 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Array of all 8-ball answer
+const answer = [
+"It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most lokely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."
+];
+
+// Updates the <p> element with 8-balls answer
+upDate = () => {
+    document.getElementById('answer').innerHTML = answer[Math.floor(Math.random() * 20 )];
+}
